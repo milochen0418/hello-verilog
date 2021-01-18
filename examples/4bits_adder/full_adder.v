2b9bb2ff46e552c8e2fa336a9279a660d32b1af1
Milo Chen <milo_chen@mail2000.com.tw>
2021-01-19 01:45:31 +0800

    Add full adder

---
+++ examples/4bits_adder/full_adder.v
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ns
+
+module full_adder(
+    input A,
+    input B,
+    input Cin,
+    output Cout,
+    output S
+    );
+
+  wire P,G,H;
+  half_adder  ha1(A,B,G,P);
+  half_adder  ha2(P,Cin,H,S);
+  or (Cout,G,H);
+endmodule
