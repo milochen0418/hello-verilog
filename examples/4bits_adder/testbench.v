d9851611fa34639d476da47256055c8011790901
Milo Chen <milo_chen@mail2000.com.tw>
2021-01-27 22:27:45 +0800

    Add test bench for 4 bits adder

---
+++ examples/4bits_adder/testbench.v
@@ -0,0 +1,31 @@
+`timescale 1ns/1ns
+
+module bit4_adderTB();
+  reg [3:0] A;
+  reg [3:0] B;
+  wire [4:0] C;
+  
+  bit4_adder FBA(
+    .A(A),
+    .B(B),
+    .Cout(C[4]),
+    .S(C[3:0])
+    );
+  
+  initial begin 
+    $dumpfile("dump.vcd"); 
+    $dumpvars(1,bit4_adderTB);  
+    #100  A=4'd3; B=4'd4;
+    #100  A=4'd0; B=4'd5;
+    #100  A=4'd9; B=4'd2;
+    #100  A=4'd10; B=4'd10;
+    #100  A=4'd15; B=4'd15;
+    #100  $finish;
+  end
+endmodule
+
+
+
+
+
+
