47540aaa0d4c5e26cfb37ca2968635b0bc5352ec
milochen0418 <milo_chen@mail2000.com.tw>
2021-03-04 00:39:45 +0800

    blocking_vs_nonblocking example

---
+++ examples/blocking_vs_nonblocking/dummy.v
@@ -0,0 +1,6 @@
+`timescale 1ns/1ns
+
+module dummy();
+endmodule
+
+
---
+++ examples/blocking_vs_nonblocking/testbench.v
@@ -0,0 +1,33 @@
+`timescale 1ns/1ns
+
+module concurrent();
+  reg [7:0] x;
+  reg [7:0] y;
+  reg [7:0] z;
+  reg CLK;
+  
+  initial begin 
+    CLK=0;
+    x=10;
+    y=10;
+    z=10;
+    $dumpfile("./out/dump.vcd"); 
+    $dumpvars(1,concurrent);  
+    #100  $finish;
+  end
+  
+  always #5 CLK = ~CLK;
+  
+  always @ (posedge CLK) begin
+    x<=25;   // 0x19
+    y<=x+13; // 0x26
+    z<=x*y;  // 0xB6
+  end
+  
+endmodule
+
+
+
+
+
+
